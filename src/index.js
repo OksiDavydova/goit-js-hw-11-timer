703e19865c1afe267a6769cab48b5f0552750e27
OksiDavydova <oksana.acva@gmail.com>
2021-08-14 14:46:09 +0300

    timerDown

--- src/index.js
+++ src/index.js
@@ -1,33 +1,59 @@
-// import './sass/main.scss';
+import './sass/main.scss';
 
-new CountdownTimer({
+class CountdownTimer {
+  constructor({selector, targetDate}) {
+    this.selector = selector;
+    this.targetDate=targetDate;
+    this.idInterval = null;
+    // this.startTimer();
+    // this.startTimer= this.startTimer.bind(this);
+  }
+  getRefs(){
+    const container = document.querySelector(this.selector);
+    const daysRef = container.querySelector('[data-value="days"]');
+    const hoursRef = container.querySelector('[data-value="hours"]');
+    const minsRef = container.querySelector('[data-value="mins"]');
+    const secsRef = container.querySelector('[data-value="secs"]');
+    const btnRef = document.querySelector('.btn');
+        return {container, daysRef, hoursRef, minsRef, secsRef, btnRef};
+  }
+
+  updateTimer({container, daysRef, hoursRef, minsRef, secsRef, btnRef}){
+// btnRef.addEventListener('click', this.startTimer);
+    const time = this.targetDate - Date.now();
+
+   if (time < 0) {
+     clearInterval(this.idInterval);
+     container.innerHTML = `<h1 class="text">Time is over</h1>`;
+    //  btnRef.removeEventListener('click', this.startTimer);
+     return;
+   }
+
+    daysRef.textContent = Math.floor(time / (1000 * 60 * 60 * 24)).toString().padStart(2,'0');
+    hoursRef.textContent = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toString().padStart(2,'0');
+    minsRef.textContent = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)).toString().padStart(2,'0');
+    secsRef.textContent = Math.floor((time % (1000 * 60)) / 1000).toString().padStart(2,'0');
+
+
+  }
+
+  startTimer() {
+    // console.log(this);
+    this.idInterval= setInterval(() => {
+      this.updateTimer(this.getRefs());
+    }, 1000);
+  }
+}
+
+const timer = new CountdownTimer({
   selector: '#timer-1',
-  targetDate: new Date('Jul 17, 2019'),
+  targetDate: new Date('Dec 31, 2021 24:00'),
 });
+// const timer2 = new CountdownTimer({
+//   selector: '#timer-2',
+//   targetDate: new Date('Oct 3, 2021'),
+// });
+timer.updateTimer(timer.getRefs());
+timer.startTimer();
 
-
-/*
- * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
- * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
- */
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
-
-/*
- * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
- * остатка % и делим его на количество миллисекунд в одном часе
- * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
- */
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
-/*
- * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
- * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
- */
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-
-/*
- * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
- * миллисекунд в одной секунде (1000)
- */
-const secs = Math.floor((time % (1000 * 60)) / 1000);
 
